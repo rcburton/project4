9b8aaca9481d8c340864a0eb8a5fa4d822db513e
rshayan <32804518+rshayan@users.noreply.github.com>
2018-12-01 12:42:07 -0500

    Add files via upload

--- VarLeafNode.java
+++ VarLeafNode.java
@@ -77,101 +77,103 @@ public class VarLeafNode
 
     public void insert(Item tem)
     {
+        if (size >= 3 && isSplitNeeded(tem)) {
+            //split and reinsert
+        }
+        else {
+            //just add item at val[size]
+        }
 
     }
 
 
-    public boolean isSplitNeeded()
+    public boolean isSplitNeeded(Item temp)
     {
         boolean intersect = false;
         boolean xIntersect = false;
         boolean yIntersect = false;
         boolean zIntersect = false;
-        while (!intersect)
+        for (int i = 0; i < size; i++)
         {
-            for (int i = 0; i < size; i++)
+
+            Item i1 = val[i];
+
+            Item j1 = temp;
+            Item a;
+            Item b;
+            // calculate intersection box
+            if (i1.getAirObj().getXorig() < j1.getAirObj().getXorig())
+            {
+                a = j1;
+                b = i1;
+            }
+            else
+            {
+                a = i1;
+                b = j1;
+            }
+
+            if ((a.getAirObj().getXorig() + a.getAirObj().getXwidth()
+                - b.getAirObj().getXorig()) <= (a.getAirObj().getXwidth()
+                    + b.getAirObj().getXwidth()))
+            {
+                xIntersect = true;
+            }
+
+            if (i1.getAirObj().getYorig() < j1.getAirObj().getYorig())
             {
 
-                Item i1 = val[i];
-                for (int j = i; j < size; j++)
+                // calculate intersection box
+                if (i1.getAirObj().getYorig() < j1.getAirObj().getYorig())
                 {
-                    Item j1 = val[j];
-                    Item a;
-                    Item b;
-                    // calculate intersection box
-                    if (i1.getAirObj().getXorig() < j1.getAirObj().getXorig())
-                    {
-                        a = j1;
-                        b = i1;
-                    }
-                    else
-                    {
-                        a = i1;
-                        b = j1;
-                    }
-
-                    if ((a.getAirObj().getXorig() + a.getAirObj().getXwidth()
-                        - b.getAirObj().getXorig()) <= (a.getAirObj()
-                            .getXwidth() + b.getAirObj().getXwidth()))
-                    {
-                        xIntersect = true;
-                    }
-
-                    if (i1.getAirObj().getYorig() < j1.getAirObj().getYorig())
-                    {
-
-                        // calculate intersection box
-                        if (i1.getAirObj().getYorig() < j1.getAirObj()
-                            .getYorig())
-                        {
-                            a = j1;
-                            b = i1;
-                        }
-                        else
-                        {
-                            a = i1;
-                            b = j1;
-                        }
-                        if ((a.getAirObj().getYorig()
-                            + a.getAirObj().getYwidth()
-                            - b.getAirObj().getYorig()) <= (a.getAirObj()
-                                .getYwidth() + b.getAirObj().getYwidth()))
-                        {
-                            yIntersect = true;
-                        }
-                    }
-                    if (i1.getAirObj().getZorig() < j1.getAirObj().getZorig())
-                    {
-
-                        // calculate intersection box
-                        if (i1.getAirObj().getZorig() < j1.getAirObj()
-                            .getZorig())
-                        {
-                            a = j1;
-                            b = i1;
-                        }
-                        else
-                        {
-                            a = i1;
-                            b = j1;
-                        }
-                        if ((a.getAirObj().getZorig()
-                            + a.getAirObj().getZwidth()
-                            - b.getAirObj().getZorig()) <= (a.getAirObj()
-                                .getZwidth() + b.getAirObj().getZwidth()))
-                        {
-                            zIntersect = true;
-                        }
-                    }
-
-                    intersect = (xIntersect || yIntersect || zIntersect);
-                    if (intersect)
-                    {
-                        break;
-                    }
+                    a = j1;
+                    b = i1;
+                }
+                else
+                {
+                    a = i1;
+                    b = j1;
+                }
+                if ((a.getAirObj().getYorig() + a.getAirObj().getYwidth()
+                    - b.getAirObj().getYorig()) <= (a.getAirObj().getYwidth()
+                        + b.getAirObj().getYwidth()))
+                {
+                    yIntersect = true;
                 }
             }
+            if (i1.getAirObj().getZorig() < j1.getAirObj().getZorig())
+            {
+
+                // calculate intersection box
+                if (i1.getAirObj().getZorig() < j1.getAirObj().getZorig())
+                {
+                    a = j1;
+                    b = i1;
+                }
+                else
+                {
+                    a = i1;
+                    b = j1;
+                }
+                if ((a.getAirObj().getZorig() + a.getAirObj().getZwidth()
+                    - b.getAirObj().getZorig()) <= (a.getAirObj().getZwidth()
+                        + b.getAirObj().getZwidth()))
+                {
+                    zIntersect = true;
+                }
+            }
+
+            intersect = (xIntersect || yIntersect || zIntersect);
+            if (!intersect)
+            {
+                break;
+            }
+            intersect = false;
+            xIntersect = false;
+            yIntersect = false;
+            zIntersect = false;
         }
-        return intersect;
+
+        return !intersect;
     }
 }
